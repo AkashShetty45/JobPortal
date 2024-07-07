6b764de88d72e71ac7d2fc879c07fe3d11a57b27
Dineshsut07 <120447913+Dineshsut07@users.noreply.github.com>
2024-07-07 16:28:07 +0530

    new branch

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Components/Home';
-import Jobs from './Components/Jobs';
-import About from './Components/About';
-import Contact from './Components/Contact';
-import Navbar from './Components/Navbar';
-import LoginSignup from './Components/login';
-import Profile from './Components/Profile';
-
+import './App.css';
+import Home from './components/Home';
+import Jobs from './components/Jobs';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PostJob from './components/PostJob';
+// import SaveJobs from './components/SaveJobs';
+// import Discussion from './components/Discussion';
+import ErrorPage from './components/ErrorPage';
+import ApplyJobs from './components/ApplyJobs';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
+    <>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" exact Component={Home} />
-          <Route path="/jobs" Component={Jobs} />
-          <Route path="/about" Component={About} />
-          <Route path="/contact" Component={Contact} />
-          <Route path="/Login" Component={LoginSignup}/>
-          <Route path='/profile' Component={Profile}/>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/jobs" element={<Jobs />} />
+            <Route path="/post-job" element={<PostJob />} />
+            <Route path="/apply-jobs" element={<ApplyJobs />} />
+            {/* <Route path="/saved-job" element={<SaveJobs />} />/ */}
+            {/* <Route path="/discussion" element={<Discussion />} /> */}
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
         </Routes>
-      </div>
-    </Router>
+      </BrowserRouter>
+    </>
   );
 }
 
